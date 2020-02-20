8c9cb43a477ac719b4debce38c0cd2a02d452839
willbridges <will.p.bridges@gmail.com>
2020-02-19 19:47:40 -0500

    almost done, still some glitches

--- Assignment3_ModelParser/BasicWidget.h
+++ Assignment3_ModelParser/BasicWidget.h
@@ -3,6 +3,7 @@
 #include <QtGui>
 #include <QtWidgets>
 #include <QtOpenGL>
+#include "ModelParser.h"
 
 /**
  * This is just a basic OpenGL widget that will allow a change of background color.
@@ -12,6 +13,27 @@ class BasicWidget : public QOpenGLWidget, protected QOpenGLFunctions
   Q_OBJECT
 
 private:
+	ModelParser bunny;
+	ModelParser monkey;
+	QVector<GLuint> indices;
+	QVector<GLfloat> vertices;
+	QVector<GLfloat> normals;
+
+	QOpenGLBuffer vbo_;
+	QOpenGLBuffer nbo_;
+	QOpenGLBuffer ibo_;
+	QOpenGLShaderProgram shaderProgram_;
+	QOpenGLVertexArrayObject vao_;
+
+	QString vertexShaderString() const;
+	QString fragmentShaderString() const;
+	void createShader();
+
+	QMatrix4x4 model_;
+	QMatrix4x4 view_;
+	QMatrix4x4 projection_;
+
+	GLenum fillMode = GL_FILL;
 
 protected:
   // Required interaction overrides
